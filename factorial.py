bdbbbf305acb57e340adfedb2a573dab531e96a8
Vishal G.Aswar <43945347+VishalAswar123@users.noreply.github.com>
2019-08-23 12:42:19 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,20 +2,16 @@
 
 #first we have a take input for find factorail number 
 #so using input() function we take input form user 
-
+#since we take input and store into left variable num 
 num = input("Enter number :\n")
 
-#since we take input and store into left variable num 
-
 #and another variable take factorial  and assign value 1 
-
 factorial = 1
 
 #and after that use for loop 
-
 for i in range(1,num+1)
+    #Logic of find factorial of number
     factorial= factorial*i
     
 #after use logic of factorial num print factorial number
-
 print("Factorial of",num, "is",factorial)
