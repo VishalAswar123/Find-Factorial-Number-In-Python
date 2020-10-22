06920eb5ff6715e0444dc57dbc191a237ca68a0d
Adarsh Addee <59617563+adarshaddee@users.noreply.github.com>
2020-10-22 19:17:15 +0530

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,17 +1,14 @@
-#In this file we write code for find a factorial of a number in python so lets do it
+choice = int(input("Enter your choice: "))
 
-#first we have a take input for find factorail number 
-#so using input() function we take input form user 
-#since we take input and store into left variable num 
-num = input("Enter number :\n")
+def fact(num):
+	
+	if num == 0 or num == 1:
+		return 1
+		
+	else:
+		return num * fact(num - 1)
+		
+print(fact(choice))
 
-#and another variable take factorial  and assign value 1 
-factorial = 1
 
-#and after that use for loop 
-for i in range(1,num+1)
-    #Logic of find factorial of number
-    factorial= factorial*i
-    
-#after use logic of factorial num print factorial number
-print("Factorial of",num, "is",factorial)
+
